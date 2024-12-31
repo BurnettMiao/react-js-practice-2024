1931c65428e84ae9892f51b0f19d8b221955ed45
Burnett Miao <searchfor1987@gmail.com>
2024-12-31 16:39:07 +0800

    props finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@
 // import Food from './Food.jsx';
 // import Footer from './Footer.jsx'
 // import Header from './Header.jsx'
-import Button from './Button/Button.jsx';
+// import Button from './Button/Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
   return (
     <>
-      <Button />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student />
     </>
   );
 }
